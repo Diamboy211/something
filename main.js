12760113eeb5919161c82e2eafb26213de4c5580
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-06-19 20:37:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var num = new Decimal(4)
 var base = new Decimal(1.66)
 
 function updateGUI() {
-  num = base.tetrate(expo)
+  num = base.tetrate(base.pow(expo))
   expo += 0.0005;
   document.getElementById("num").textContent = "number: " + num
 }
