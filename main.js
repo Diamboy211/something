3cda0cf3f45b96506ec32207705a29ed42739fbc
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-06-19 13:07:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var expo = 0
+var expo = -12
 var num = new Decimal(4)
 var base = new Decimal(1.01)
 
 function updateGUI() {
   num = base.pow(base.pow(base.pow(base.pow(base.pow(base.pow(expo))))));
-  expo += 0.004;
+  expo += 0.00004;
   document.getElementById("num").textContent = "number: " + num
 }
 
